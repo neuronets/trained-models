0b942c10b3582ca63c473c72e95508c097ac9cea
Nicholas Gustafson <nickgust@mit.edu>
2022-11-22 22:38:46 -0700

    script to scrape model data

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -1,1 +1,34 @@
-console.log('ran');
+const fs = require('fs');
+const path = require("path");
+const yaml = require('js-yaml');
+
+const ignoreDirs = [
+    '.datalad', '.github', 'docs', 'images', 'weights'
+];
+
+const getAllPaths = function(dirPath, arrayOfPaths) {
+    files = fs.readdirSync(dirPath);
+
+    arrayOfPaths = arrayOfPaths || [];
+  
+    files.forEach(function(file) {
+      if (fs.statSync(dirPath + "/" + file).isDirectory() && !ignoreDirs.includes(file)) {
+        arrayOfPaths = getAllPaths(dirPath + "/" + file, arrayOfPaths);
+      } else if (file === 'spec.yaml') {
+        arrayOfPaths.push(path.join(dirPath, "/", file));
+      }
+    });
+  
+    return arrayOfPaths;
+}
+
+const paths = getAllPaths('.');
+const models = [];
+paths.forEach(function(path) {
+    const doc = yaml.load(fs.readFileSync(path, 'utf8'));
+    models.push(doc.model);
+});
+yamlModels = yaml.dump(models);
+fs.writeFile("./docs/_data/models.yml", yamlModels, "utf8", err => {
+    if (err) console.log(err);
+});
