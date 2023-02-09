eb9ea7b27562fe6d304828a81662ae62e0453179
Satrajit Ghosh <satra@mit.edu>
2023-02-08 21:04:20 -0500

    ENH: model list visualization (#46)
    * initialized jekyll"
    
    * removed unnecessary docs/ in root and renamed site/ to docs/
    
    * created new workflow
    
    * changed script.ts to js
    
    * script to scrape model data
    
    * populate index with model names
    
    * git status to workflow
    
    * add _data to repo
    
    * push git changes in wokrflow
    
    * Auto updating docs/_data/models.yml
    
    * more info per model
    
    * cleared _data/models.yml
    
    * Auto updating docs/_data/models.yml
    
    * only add/commit/push if changes
    
    * formatting bug in get_model_data
    
    * model page styling
    
    * architecture for model cards
    
    * changed script to support new architecture
    
    * auto create page for each model
    
    * fixed script bug
    
    * rerun workflow
    
    * Auto updating docs/
    
    * test push
    
    * fixed links
    
    * Revert "fixed links"
    
    This reverts commit 69a3dabf5cb65c4bf8f7a86ad320e7f99417f5b2.
    
    * Revert "Revert "fixed links""
    
    This reverts commit 6a90ffd75380c6d45381ebdd5fd9ec4e4071b798.
    
    * styling on model cards
    
    * new index architecture
    
    * working links with new architecture
    
    * collapsible
    
    * collapse logic
    
    * collapsibles working
    
    * collapse/expand minus link funct
    
    * nav styling
    
    * nonfunctional searchbar
    
    * styled org
    
    * collapse styling
    
    * link funcitonality
    
    * card styling
    
    * version bug
    
    * files
    
    * plus minus
    
    * search functionality
    
    * model description info
    
    * styling
    
    * implemented new script
    
    * modified script
    
    * get_model_data bug
    
    * adding data files
    
    * adding model files
    
    * version is clickable link
    
    * default model card on home
    
    * updated script to search for blank val in specs
    
    * modified spec.yml file for each model
    
    * models doc
    
    * removed node_modules from repo
    
    * removed files
    
    * returned _data dir
    
    * ignore package.json + package-lock.json
    
    * install dependencies from github actions
    
    * set configs in actions
    
    * runs to run in actions
    
    * adding package and package-lock to repo
    
    * github actions update
    
    * sleep after call
    
    * macos github actions
    
    * ubuntu github actions
    
    * remove path filter
    
    * ubuntu retest
    
    * remove path filter
    
    * git auto commit
    
    * npm ci
    
    * move package and package-lock
    
    * napm ci -w ...
    
    * string path
    
    * move package and package lock back to root
    
    * Apply automatic changes
    
    * using template repo, push to gh-pages
    
    * mkdir, cp instead of mv
    
    * removed docs dir from master
    
    * hide package-lock.json
    
    * readd package
    
    * mv package in workflow
    
    * using jekyll action
    
    * fixed target branch and added cache
    
    * remove jekyll_src
    
    ---------
    
    Co-authored-by: Nicholas Gustafson <nickgust@mit.edu>
    Co-authored-by: runner <runner@Mac-1669182707373.local>
    Co-authored-by: runner <runner@Mac-1669221232120.local>
    Co-authored-by: runner <runner@Mac-1669785210012.local>
    Co-authored-by: ngustafson2023 <ngustafson2023@users.noreply.github.com>

---
+++ .github/workflows/get_model_data.js
@@ -0,0 +1,128 @@
+const fs = require('fs');
+const path = require("path");
+const yaml = require('js-yaml');
+
+const ignoreDirs = [
+    '.datalad', '.github', 'docs', 'images', 'weights', 'trained-models-template'
+];
+
+// find all spec.yml files
+const getAllPaths = function(dirPath, arrayOfPaths) {
+    files = fs.readdirSync(dirPath);
+    arrayOfPaths = arrayOfPaths || [];
+  
+    files.forEach(function(file) {
+      if (fs.statSync(dirPath + "/" + file).isDirectory() && !ignoreDirs.includes(file)) {
+        arrayOfPaths = getAllPaths(dirPath + "/" + file, arrayOfPaths);
+      } else if (file === 'spec.yaml') {
+        arrayOfPaths.push(path.join(dirPath, "/", file));
+      }
+    });
+  
+    return arrayOfPaths;
+}
+
+function findObj(name, arr) {
+  for (const obj of arr) {
+    if (obj.name === name) return obj;
+  }
+  return null;
+}
+
+const paths = getAllPaths('.');
+const names = [];
+models = {};
+
+paths.forEach(function(path) {
+  const doc = yaml.load(fs.readFileSync(path, 'utf8'));
+  
+  //create names.yml
+  const example = doc.model.example.split(' ');
+  let org;
+  let modelName;
+  let version;
+  let isLink;
+  let modelType;
+  for (let i=0; i < example.length; i++) {
+    const str = example[i];
+    if (str.includes(doc.model.model_name)) {
+      const initCombinedName = str.split('/');
+      org = initCombinedName[0];
+      modelName = initCombinedName[1];
+      version = initCombinedName[2];
+      isLink = !(example[i+1] === '--model_type');
+      modelType = !isLink ? example[i+2] : 'model';
+      break;
+    }
+  }
+  let orgStruct = findObj(org, names);
+  if (orgStruct === null) {
+    orgStruct = {
+      name: org,
+      modelNames: []
+    };
+    names.push(orgStruct);
+  }
+  let modelNameStruct = findObj(modelName, orgStruct.modelNames);
+  if (modelNameStruct === null) {
+    modelNameStruct = {
+      name: modelName,
+      versions: []
+    };
+    orgStruct.modelNames.push(modelNameStruct);
+  }
+  let versionStruct = findObj(version, modelNameStruct.versions);
+  if (versionStruct === null) {
+    versionStruct = {
+      name: version,
+      modelTypes: []
+    };
+    versionStruct.isLink = isLink;
+    modelNameStruct.versions.push(versionStruct);
+  }
+  if (!isLink) {
+    versionStruct.modelTypes.push({
+      name: modelType
+    });
+  }
+
+  // create models.yml
+  const combined_name = org + '_' + modelName + '_' + version + '_' + modelType;
+  models[combined_name] = doc.model;
+  modelCardFields = [
+    'model_details', 'intended_use', 'factors', 'metrics', 'eval_data', 'training_data', 'quant_analyses', 'ethical_considerations', 'caveats_recs'
+  ];
+  for (const field of modelCardFields) {
+    if (models[combined_name][field] === '') {
+      models[combined_name][field] = 'Information not provided.';
+    }
+  }
+
+  // create model pages
+  const permalink = `/${org}/${modelName}/${version}/${modelType}/`;
+  const page = `---
+  layout: model_card
+  permalink: ${permalink}
+  combined_name: ${combined_name}
+  org: ${org}
+  modelName: ${modelName}
+  version: ${version}
+  modelType: ${modelType}
+---
+  `;
+  const filename = `./trained-models-template/docs/_pages/${combined_name}.markdown`;
+  fs.writeFile(filename, page, "utf8", err => {
+    if (err) console.log(err);
+  });
+});
+
+// write to files
+yamlNames = yaml.dump(names);
+fs.writeFile("./trained-models-template/docs/_data/names.yml", yamlNames, "utf8", err => {
+  if (err) console.log(err);
+});
+
+yamlModels = yaml.dump(models);
+fs.writeFile("./trained-models-template/docs/_data/models.yml", yamlModels, "utf8", err => {
+    if (err) console.log(err);
+});
