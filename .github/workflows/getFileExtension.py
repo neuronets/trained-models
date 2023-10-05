9acead04eef5d111417bdb04077fce982639a79f
EDWARD GAIBOR <edward.gaibor001@umb.edu>
2023-10-05 13:15:42 -0400

    gt5

--- .github/workflows/getFileExtension.py
+++ .github/workflows/getFileExtension.py
@@ -1,4 +1,18 @@
 import requests
+import sys
 
-response = requests.head("https://github.com/neuroneural/DeepCSR-fork/blame/master/src/utils.py")
-print(response.headers['Content-Type'])
+# Get URL from standard input when the python file is run
+url = sys.argv[1]
+
+response = requests.get(url)
+
+# Get the filename from the response headers
+content_type = response.headers["Content-Disposition"]
+# Get the file extension
+extension = content_type.split("=")[1].split(";")[0]
+
+# Return everything after the first "." and without the last quote
+extension = extension[1:].strip('"')
+extension = extension[extension.find(".") + 1:]
+
+print(extension)
